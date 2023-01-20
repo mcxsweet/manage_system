20e43b4e3b6ae0b48b876f82feae22aa08037fa5
LAgility <80461306+2859377869@users.noreply.github.com>
2023-01-20 13:38:59 +0800

    Add files via upload

---
+++ src/components/classInformation.vue
@@ -0,0 +1,165 @@
+<template>
+    <el-container>
+           
+        <el-main>
+           <el-form :model="FormData" label-width="100px" >
+                <el-form-item label="课程名称" prop="courseName">
+                    <el-input v-model="FormData.courseName"></el-input>
+                </el-form-item>
+                <el-form-item label="任课教师" prop="classroomTeacher">
+                    <el-input v-model="FormData.classroomTeacher"></el-input>
+                </el-form-item>
+                <el-form-item label="理论学时" prop="theoreticalHours">
+                    <el-input v-model="FormData.theoreticalHours"></el-input>
+                </el-form-item>
+                <el-form-item label="实验学时" prop="labHours">
+                    <el-input v-model="FormData.labHours"></el-input>
+                </el-form-item>
+                <el-form-item label="班级名称" prop="className">
+                    <el-input v-model="FormData.className"></el-input>
+                </el-form-item>
+                <el-form-item label="学期" prop="term">
+                    <el-input v-model="FormData.term"></el-input>
+                    <!-- <el-date-picker v-model="FormData.term" type="year"></el-date-picker>
+                    <span style="margin: auto 20px;">至</span>
+                    <el-date-picker v-model="FormData.term" type="year"></el-date-picker> -->
+                </el-form-item>
+                <el-form-item label="学生人数" prop="studentsNum">
+                    <el-input v-model="FormData.studentsNum"></el-input>
+                </el-form-item>
+                <el-form-item label="课程性质" prop="courseNature">
+                    <el-select v-model="FormData.courseNature" placeholder="请选择课程性质">
+                        <el-option label="必修" value="必修"></el-option>
+                        <el-option label="选修" value="选修"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="课程类别" prop="courseType">
+                    <el-input v-model="FormData.courseType"></el-input>
+                </el-form-item>
+                    <el-form-item :inline="true" label="课程目标数量" prop="courseTargetNum" width="80%">
+                        <template solt-scope="scope">
+                            <!-- <span>{{ scope.FormData.courseTargetNum}}</span> -->
+                            <el-input v-model="FormData.courseTargetNum"></el-input>
+                            <router-link :to="{
+                                    path:'/MainPage/classInformation/programObjective/',
+                                    query:{
+                                        id:FormData.courseTargetNum
+                                    }
+                            } " tag="el-button" @click.native="settingbegain">设置课程目标</router-link>
+                            <el-button type="primary" v-show="showbt" @click="settingOver">设置完毕</el-button>
+                            <router-view v-show="showObjective"></router-view>
+                        </template>
+                    </el-form-item>
+                <!-- <el-form-item label="课程目标数量" prop="courseTargetNum" >
+                    <el-input v-model="FormData.courseTargetNum"></el-input>
+                    <el-form-item><el-button>设置课程目标</el-button></el-form-item>
+                </el-form-item> -->
+                <el-form-item label="指标点数量" prop="indicatorPointsNum">
+                    <el-input v-model="FormData.indicatorPointsNum"></el-input>
+                </el-form-item>
+                <el-form-item label="指标点编号" prop="indicatorPoints">
+                    <el-input v-model="FormData.indicatorPoints"></el-input>
+                </el-form-item>
+            </el-form>
+
+        </el-main>
+    <el-footer>
+        <el-row style="margin-left: 40%;">
+            <el-button type="primary" @click="goto('courseBasicInformation')">返回</el-button>
+            <el-button type="danger">保存</el-button>   
+        </el-row>
+    </el-footer>
+    </el-container>
+    
+    
+</template>
+
+<script>
+import api from '@/api/api'
+import programObjection from './programObjective.vue'
+export default {
+    
+    name:"classInformation",
+    data(){
+        return{
+            tableData:[
+                {className: "计算,机科学与技术2021",
+                classroomTeacher: "阳老师",
+                courseName: "高数",
+                courseNature: "必修",
+                courseTargetNum: 5,
+                courseType: "专业必修课",
+                indicatorPoints: "指标1,指标2",
+                indicatorPointsNum: 2,
+                labHours: 4,
+                studentsNum: 80,
+                term: "2022-2023.1",
+                theoreticalHours: 16,},
+                {className: "计算,机科学与技术2022",
+                classroomTeacher: "阳老师",
+                courseName: "高数",
+                courseNature: "必修",
+                courseTargetNum: 5,
+                courseType: "专业必修课",
+                indicatorPoints: "指标1,指标2",
+                indicatorPointsNum: 2,
+                labHours: 4,
+                studentsNum: 80,
+                term: "2022-2023.1",
+                theoreticalHours: 16,},
+                {className: "计算,机科学与技术2020",
+                classroomTeacher: "阳老师",
+                courseName: "高数",
+                courseNature: "必修",
+                courseTargetNum: 5,
+                courseType: "专业必修课",
+                indicatorPoints: "指标1,指标2",
+                indicatorPointsNum: 2,
+                labHours: 4,
+                studentsNum: 80,
+                term: "2022-2023.1",
+                theoreticalHours: 16,}
+            ],
+            FormData: {
+                className: "计算,机科学与技术2020",
+                classroomTeacher: "阳老师",
+                courseName: "高数",
+                courseNature: "必修",
+                courseTargetNum: 5,
+                courseType: "专业必修课",
+                indicatorPoints: "指标1,指标2",
+                indicatorPointsNum: 2,
+                labHours: 4,
+                studentsNum: 80,
+                term: "2022-2023.1",
+                theoreticalHours: 16,
+            },
+            props:{courseTargetNum:{
+                type:Number,
+                Required:true
+            }},
+            showbt:false,
+            showObjective:true
+        }
+    },
+    methods:{
+        goto(url){
+            this.$router.push({ path: '/MainPage/' + url });
+        },
+        settingbegain() {
+            this.showbt = true,
+            this.showObjective=true
+        },
+         settingOver(){
+             this.showObjective = !this.showObjective,
+             this.showbt=false
+         }
+    },
+    components:{
+        programObjection
+    }
+}
+</script>
+   
+<style>
+</style>
---
+++ src/components/programObjective.vue
@@ -0,0 +1,113 @@
+<template>
+  <el-container>
+    <el-main>
+      <el-table :data="tableData" border style="width: 100%">
+        <!-- <el-table-column prop="id" label="序号" width="80%">
+          <template slot-scope="scope"> -->
+            <!-- <input v-model="scope.row.id" v-show="scope.row.ised"/> -->
+            <!-- <span>{{ scope.row.id }}</span>
+          </template>
+        </el-table-column> -->
+        <el-table-column prop="name" label="课程目标">
+          <template slot-scope="scope">
+            <textarea  v-model="scope.row.name" v-show="scope.row.ised"></textarea>
+            <span v-show="!scope.row.ised">{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="address" label="毕业要求指标点">
+          <template slot-scope="scope">
+            <textarea v-model="scope.row.address" v-show="scope.row.ised"></textarea>
+            <span v-show="!scope.row.ised">{{ scope.row.address }}</span>
+          </template>
+         </el-table-column>
+         <el-table-column prop="approach" label="达成途径">
+            <template slot-scope="scope">
+              <textarea v-model="scope.row.approach" v-show="scope.row.ised"></textarea>
+              <span v-show="!scope.row.ised">{{ scope.row.approach }}</span>
+            </template>
+        </el-table-column>
+        <el-table-column prop="gist" label="评价依据">
+          <template slot-scope="scope">
+              <textarea v-model="scope.row.gist" v-show="scope.row.ised"></textarea>
+              <span v-show="!scope.row.ised">{{ scope.row.gist }}</span>
+            </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="warning" @click="editta(scope.row,scope)">编辑</el-button>
+            <el-button type="danger" @click="saveta(scope.row)">保存</el-button>
+          </template>
+        </el-table-column>
+    </el-table>
+    </el-main>
+  </el-container>
+    
+    
+</template>
+
+<script>
+export default {
+    name:'programObjective',
+    data() {
+      return {
+        tableData: [{
+          id: 1,
+          name: '课程目标1：理解信号的特征、分类，理解系统的描述和分类，掌握信号的运算和连续时不变系统的性质和判断；',
+          address: '指标点1.1:掌握数学、自然科学、工程基础和电子信息专业知识的基本概念，并将其运用到复杂工程问题的适当表述之中',
+          approach:'通过课堂讲授',
+          gist:'作业、考试',
+          ised:false
+        }, {
+          id: 2,
+          name: '课程目标 2：掌握信号、线性系统的时域特征，建立电路等系统的时域数学模型，能应用时域分析方法（卷积积分）分析线性时不变系统；',
+          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
+          approach:'通过课堂讲授',
+          gist:'作业、考试',
+          ised:false
+        }, {
+          id: 3,
+          name: '课程目标 3：掌握信号、线性系统的频域特征，建立电路等系统的频域数学模型，能应用频域分析方法（傅里叶级数、傅里叶变换）分析线性时不变系统；',
+          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
+          approach:'通过课堂讲授',
+          gist:'作业、考试',
+          ised:false
+        }, {
+          id: 4,
+          name: '课程目标 4：掌握拉普拉斯变换，理解信号、系统的复频域特征，建立电路等系统的复频域数学模型，能应用复频域分析方法（拉普拉斯变换）分析线性时不变系统；',
+          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
+          approach:'通过课堂讲授',
+          gist:'作业、考试',
+          ised:false
+        },{
+          id: this.$route.query.id,
+          name: '课程目标5：掌握z变换，理解信号、系统的z域特征，建立电路等系统的z域数学模型，能应用z域分析方法分析离散线性时不变系统；',
+          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
+          approach:'通过课堂讲授',
+          gist:'作业、考试',
+          ised:false
+        }
+        ]} 
+    },
+      methods:{
+        editta(row,index){
+          row.ised = true;
+          
+        },
+        saveta(row,index){
+          row.ised = false
+        },
+        testchange(event,id){
+          const name = '王小虎';
+          const [firstId] = id;
+          const obj = {};
+          obj[firstId] = [{name,address:'上海市普陀区金沙江路'}];
+          console.log(obj)
+        }
+      }
+    }
+
+</script>
+
+<style>
+
+</style>
