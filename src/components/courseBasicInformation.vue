4acd8b12212906536157a17b159493578b7fbba0
LAgility <80461306+2859377869@users.noreply.github.com>
2023-02-02 01:34:19 +0800

    Add files via upload

--- src/components/courseBasicInformation.vue
+++ src/components/courseBasicInformation.vue
@@ -4,26 +4,27 @@
             <el-row>
                 <el-button plain @click="isShow = !isShow">添加</el-button>
                 <el-button type="danger" plain>删除</el-button>
-                <el-button type="success" plain>搜索</el-button>
+                <el-button type="success" plain @click="hunt">搜索</el-button>
                 <input type="text" plain placeholder="请输入搜索内容" class="sousuo" v-model="sousuo" @keyup.enter:>
             </el-row>
         </el-header>
 
         <el-main>
-            <el-table :data="tableData" stripe height="80vh">
-                <el-table-column align="left" width="200">
+            <el-table :data="tableData" stripe height="73vh" border="true">
+                <el-table-column align="left" width="300">
                     <template slot-scope="scope">
-                        <el-button size="mini" type="primary" round @click="goto('classInformation')">设置</el-button>
+                        <!-- <router-link tag="el-button" :to="{path:'/MainPage/classInformation',scope.$index,}">11</router-link> -->
+                        <el-button size="mini" type="primary" round @click="goto('classInformation'),handleSetting(scope.$index,scope.row)">设置</el-button>
                         <el-button size="mini" type="info"
-                            @click="handleExport(scope.$index, scope.row)">Export</el-button>
+                            @click="handleExport(scope.$index, scope.row)">导出</el-button>
                         <el-button size="mini" type="danger"
-                            @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+                            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
                     </template>
                 </el-table-column>
                 <el-table-column prop="courseName" label="课程名称" width="200">
                 </el-table-column>
-                <el-table-column prop="classroomTeacher" label="任课教师" width="140">
-                </el-table-column>
+                <!-- <el-table-column prop="classroomTeacher" label="任课教师" width="140">
+                </el-table-column> -->
                 <el-table-column prop="theoreticalHours" label="理论学时" width="100">
                 </el-table-column>
                 <el-table-column prop="labHours" label="实验学时" width="100">
@@ -38,23 +39,22 @@
                 </el-table-column>
                 <el-table-column prop="courseType" label="课程类别" width="140">
                 </el-table-column>
-                <el-table-column prop="courseTargetNum" label="课程目标数量" width="140">
+                <!-- <el-table-column prop="courseTargetNum" label="课程目标数量" width="140">
                 </el-table-column>
                 <el-table-column prop="indicatorPointsNum" label="指标点数量" width="100">
-                </el-table-column>
-                <el-table-column prop="indicatorPoints" label="指标点编号" width="140">
-                </el-table-column>
+                </el-table-column> -->
+                <!-- <el-table-column prop="indicatorPoints" label="指标点编号" width="140"> -->
+                <!-- </el-table-column> -->
             </el-table>
         </el-main>
-
         <el-dialog title="添加" :visible.sync="isShow">
             <el-form :model="FormData" label-width="100px" class="demo-ruleForm">
                 <el-form-item label="课程名称" prop="courseName">
                     <el-input v-model="FormData.courseName"></el-input>
                 </el-form-item>
-                <el-form-item label="任课教师" prop="classroomTeacher">
+                <!-- <el-form-item label="任课教师" prop="classroomTeacher">
                     <el-input v-model="FormData.classroomTeacher"></el-input>
-                </el-form-item>
+                </el-form-item> -->
                 <el-form-item label="理论学时" prop="theoreticalHours">
                     <el-input v-model="FormData.theoreticalHours"></el-input>
                 </el-form-item>
@@ -82,7 +82,7 @@
                 <el-form-item label="课程类别" prop="courseType">
                     <el-input v-model="FormData.courseType"></el-input>
                 </el-form-item>
-                <el-form-item label="课程目标数量" prop="courseTargetNum">
+                <!-- <el-form-item label="课程目标数量" prop="courseTargetNum">
                     <el-input v-model="FormData.courseTargetNum"></el-input>
                 </el-form-item>
                 <el-form-item label="指标点数量" prop="indicatorPointsNum">
@@ -90,7 +90,7 @@
                 </el-form-item>
                 <el-form-item label="指标点编号" prop="indicatorPoints">
                     <el-input v-model="FormData.indicatorPoints"></el-input>
-                </el-form-item>
+                </el-form-item> -->
             </el-form>
 
             <div slot="footer" class="dialog-footer">
@@ -98,36 +98,52 @@
                 <el-button type="primary" @click="submit">确 定</el-button>
             </div>
         </el-dialog>
+        <el-footer>
+           <span>总共有{{ tableData.length }}条课程</span>
+    </el-footer>
     </el-container>
 </template>
 
 <script>
 import api from '@/api/api'
+import axios from 'axios'
 export default {
     name: "courseBasicInformation",
     data() {
         return {
             tableData: [],
             FormData: {
-                className: "计算机科学与技术2020",
+                className: "计算,机科学与技术2020",
                 classroomTeacher: "阳老师",
                 courseName: "高数",
                 courseNature: "必修",
                 courseTargetNum: 5,
                 courseType: "专业必修课",
-                indicatorPoints: "指标1,指标2",
-                indicatorPointsNum: 2,
-                labHours: 4,
-                studentsNum: 80,
-                term: "2022-2023.1",
-                theoreticalHours: 16,
             },
-            isShow: false
+            isShow: false,
+            sousuo:''
         }
     },
+    mounted(){
+        this.gettableData();
+    },
     methods: {
+        gettableData(){
+            axios.get('http://localhost:8080/data/query')
+            .then(res=>{
+                this.tableData = res.data;
+            })
+        },
         goto(url) {
-            this.$router.push({ path: '/MainPage/' + url });
+            this.$router.push({ 
+                path: '/MainPage/' + url ,
+            });
+        },
+        //点击搜索内容
+        hunt(){
+            setTimeout(() => {
+                this.$router.push({ path: '/welcome' });
+            }, 2000);
         },
         getMessage() {
             api.get("/courseInfo", "", (resp) => {
@@ -137,33 +153,10 @@ export default {
         handleExport(index, object) {
             window.location.href = "http://localhost:8080/courseInfo/export/" + object.id;
         },
-        handleDelete(index, object) {
-            this.$confirm('是否删除 ?', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning'
-            }).then(() => {
-                api.del("/courseInfo", object, (resp) => {
-                    if (resp.data.flag) {
-                        this.$message({
-                            type: 'success',
-                            message: '删除成功!'
-                        });
-                        this.getMessage();
-                    } else if (resp.status != 200) {
-                        this.$message({
-                            type: 'error',
-                            message: '删除失败!'
-                        });
-                    }
-                })
-            }).catch(() => {
-                this.$message({
-                    type: 'info',
-                    message: '已取消'
-                });
-            });
-        },
+         handleSetting(index,object){
+            classdata:[] = object
+            this.$emit('childClick',classdata)
+         },
         submit() {
             this.$confirm('是否提交 ?', '提示', {
                 confirmButtonText: '确定',
@@ -200,9 +193,9 @@ export default {
 </script>
 
 <style>
-.sousuo {
-    padding: 10px;
-    margin: 20px;
-    width: 300px;
-}
+   .sousuo{
+        padding: 10px;
+        margin: 20px;
+        width: 300px;
+    }
 </style>
--- src/components/programObjective.vue
+++ src/components/programObjective.vue
@@ -2,43 +2,58 @@
   <el-container>
     <el-main>
       <el-table :data="tableData" border style="width: 100%">
-        <!-- <el-table-column prop="id" label="序号" width="80%">
-          <template slot-scope="scope"> -->
-            <!-- <input v-model="scope.row.id" v-show="scope.row.ised"/> -->
-            <!-- <span>{{ scope.row.id }}</span>
+        <el-table-column prop="id" label="序号" width="90%">
+          <template > 
+            <el-span v-model="id">{{ id }}</el-span>
           </template>
-        </el-table-column> -->
+        </el-table-column> 
         <el-table-column prop="name" label="课程目标">
           <template slot-scope="scope">
-            <textarea  v-model="scope.row.name" v-show="scope.row.ised"></textarea>
+            <el-input v-model="scope.row.name" v-show="scope.row.ised"></el-input>
             <span v-show="!scope.row.ised">{{ scope.row.name }}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="address" label="毕业要求指标点">
+        <el-table-column label="毕业要求指标点" width="220">
           <template slot-scope="scope">
-            <textarea v-model="scope.row.address" v-show="scope.row.ised"></textarea>
-            <span v-show="!scope.row.ised">{{ scope.row.address }}</span>
+            <el-select v-model="scope.row.value1" multiple placeholder="请选择" v-show="scope.row.ised">
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+              </el-option>
+            </el-select>
+            <div v-for="(item,index) in scope.row.value1" :key="index" v-show="!scope.row.ised">  
+              <span>{{ item }}</span>
+            </div>
           </template>
          </el-table-column>
          <el-table-column prop="approach" label="达成途径">
             <template slot-scope="scope">
-              <textarea v-model="scope.row.approach" v-show="scope.row.ised"></textarea>
+              <el-input v-model="scope.row.approach" v-show="scope.row.ised"></el-input>
               <span v-show="!scope.row.ised">{{ scope.row.approach }}</span>
             </template>
         </el-table-column>
-        <el-table-column prop="gist" label="评价依据">
+        <el-table-column label="请选择评价依据" width="100">
           <template slot-scope="scope">
-              <textarea v-model="scope.row.gist" v-show="scope.row.ised"></textarea>
-              <span v-show="!scope.row.ised">{{ scope.row.gist }}</span>
+            <el-select v-model="scope.row.value2" multiple placeholder="评价依据" v-show="scope.row.ised"> 
+                        <el-option label="考试" value="考试"></el-option>
+                        <el-option label="作业" value="作业"></el-option>
+                    </el-select>
+                    <div v-for="(item2,index) in scope.row.value2" :key="index" v-show="!scope.row.ised">  
+              <span>{{ item2 }}</span>
+            </div>
             </template>
         </el-table-column>
-        <el-table-column label="操作">
+        <el-table-column label="操作" width="220">
           <template slot-scope="scope">
-            <el-button type="warning" @click="editta(scope.row,scope)">编辑</el-button>
-            <el-button type="danger" @click="saveta(scope.row)">保存</el-button>
+            <el-button type="warning" size="mini" @click="editta(scope.row,scope)">编辑</el-button>
+            <el-button type="danger" size="mini" @click="saveta(scope.row)">保存</el-button>
+            <el-button @click="delect" size="mini">删除</el-button>
           </template>
         </el-table-column>
     </el-table>
+    <el-button icon="el-icon-plus" type="primary" @click="add">添加课程目标</el-button>
     </el-main>
   </el-container>
     
@@ -46,66 +61,61 @@
 </template>
 
 <script>
+
+
 export default {
     name:'programObjective',
     data() {
       return {
-        tableData: [{
-          id: 1,
-          name: '课程目标1：理解信号的特征、分类，理解系统的描述和分类，掌握信号的运算和连续时不变系统的性质和判断；',
-          address: '指标点1.1:掌握数学、自然科学、工程基础和电子信息专业知识的基本概念，并将其运用到复杂工程问题的适当表述之中',
-          approach:'通过课堂讲授',
-          gist:'作业、考试',
-          ised:false
+        newObj:[],
+        js:0,
+        kecheng:'课程目标',
+         obj:{
+           id:'',
+           name:'',   
+           approach:'',
+           ised:true
+         },
+        tableData: [],
+         options: [{
+          value: '选项1',
+          label: '指标的1'
         }, {
-          id: 2,
-          name: '课程目标 2：掌握信号、线性系统的时域特征，建立电路等系统的时域数学模型，能应用时域分析方法（卷积积分）分析线性时不变系统；',
-          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
-          approach:'通过课堂讲授',
-          gist:'作业、考试',
-          ised:false
+          value: '选项2',
+          label: '指标的2'
         }, {
-          id: 3,
-          name: '课程目标 3：掌握信号、线性系统的频域特征，建立电路等系统的频域数学模型，能应用频域分析方法（傅里叶级数、傅里叶变换）分析线性时不变系统；',
-          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
-          approach:'通过课堂讲授',
-          gist:'作业、考试',
-          ised:false
+          value: '选项3',
+          label: '指标的3'
         }, {
-          id: 4,
-          name: '课程目标 4：掌握拉普拉斯变换，理解信号、系统的复频域特征，建立电路等系统的复频域数学模型，能应用复频域分析方法（拉普拉斯变换）分析线性时不变系统；',
-          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
-          approach:'通过课堂讲授',
-          gist:'作业、考试',
-          ised:false
-        },{
-          id: this.$route.query.id,
-          name: '课程目标5：掌握z变换，理解信号、系统的z域特征，建立电路等系统的z域数学模型，能应用z域分析方法分析离散线性时不变系统；',
-          address: '指标点1.2:针对一个复杂系统或者过程选择恰当的数学模型，对模型正确性进行严谨的推理，并能正确求解',
-          approach:'通过课堂讲授',
-          gist:'作业、考试',
-          ised:false
-        }
-        ]} 
+          value: '选项4',
+          label: '指标的4'
+        }, {
+          value: '选项5',
+          label: '指标的5'
+        }],
+        value1: [],
+        value2:[]
+      } 
     },
+    computed:{
+          id(){
+            return '课程目标'+this.js+':'
+          } ,
+         
+        },
       methods:{
         editta(row,index){
           row.ised = true;
-          
         },
         saveta(row,index){
           row.ised = false
         },
-        testchange(event,id){
-          const name = '王小虎';
-          const [firstId] = id;
-          const obj = {};
-          obj[firstId] = [{name,address:'上海市普陀区金沙江路'}];
-          console.log(obj)
+        add(){
+          this.tableData.push(JSON.parse(JSON.stringify(this.obj)))
+          return this.js = this.js+1
         }
       }
     }
-
 </script>
 
 <style>
