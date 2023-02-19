564ce6afe04b9942fcc400b428673e42f97fd0b4
LAgility <80461306+2859377869@users.noreply.github.com>
2023-02-20 01:15:51 +0800

    Update MainPage.vue

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -20,45 +20,36 @@
                         <el-menu-item-group>
                             <template slot="title">分组一</template>
                             <el-menu-item index="1-1" @click="goto('courseBasicInformation')">课程基本信息</el-menu-item>
-                            <el-menu-item index="1-2">选项2</el-menu-item>
-                        </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
-                            <el-menu-item index="1-3">选项3</el-menu-item>
-                        </el-menu-item-group>
+                             <el-menu-item index="1-2" @click="goto('basicInformationTable')">课程期末基本信息表</el-menu-item>
+                        </el-menu-item-group>                       
+                            <el-menu-item index="1-3">课程目标</el-menu-item>
                         <el-submenu index="1-4">
-                            <template slot="title">选项4</template>
+                            <template slot="title">课程目标</template>
                             <el-menu-item index="1-4-1">选项4-1</el-menu-item>
                         </el-submenu>
                     </el-submenu>
                     <el-submenu index="2">
-                        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-                        <el-menu-item-group>
-                            <template slot="title">分组一</template>
-                            <el-menu-item index="2-1">选项1</el-menu-item>
-                            <el-menu-item index="2-2">选项2</el-menu-item>
+                        <template slot="title"><i class="el-icon-menu"></i>试卷管理</template>
+                        <el-menu-item-group>    
+                            <el-menu-item index="2-1" @click="goto('finalTable')">课程期末考试命题与课程目标、指标点分数对应表</el-menu-item>
                         </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
-                            <el-menu-item index="2-3">选项3</el-menu-item>
-                        </el-menu-item-group>
-                        <el-submenu index="2-4">
-                            <template slot="title">选项4</template>
-                            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-                        </el-submenu>
                     </el-submenu>
                     <el-submenu index="3">
-                        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+                        <template slot="title"><i class="el-icon-setting"></i>成绩管理</template>
                         <el-menu-item-group>
-                            <template slot="title">分组一</template>
-                            <el-menu-item index="3-1">选项1</el-menu-item>
-                            <el-menu-item index="3-2">选项2</el-menu-item>
+                            <template slot="title">平时成绩</template>
+                            <el-menu-item index="3-1" @click="goto('usualPreformanceTable')">平时考核成绩统计表</el-menu-item>
+                            <el-menu-item index="3-2" @click="goto('finalStatisticsTable')">课程期末试卷成绩</el-menu-item>
                         </el-menu-item-group>
                         <el-menu-item-group title="分组2">
-                            <el-menu-item index="3-3">选项3</el-menu-item>
+                            <el-menu-item index="3-3" @click="goto('finalComprehensiveTable')">期末综合成绩统计表</el-menu-item>
                         </el-menu-item-group>
                         <el-submenu index="3-4">
-                            <template slot="title">选项4</template>
-                            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+                            <template slot="title">成绩分析报告</template>
+                            <el-menu-item index="3-4-1" @click="goto('analysisTable')">期末综合成绩分析表</el-menu-item>
+                            <el-menu-item index="3-4-1" @click="goto('programTable')">课程达成度成绩计算表</el-menu-item>
                         </el-submenu>
+
                     </el-submenu>
                 </el-menu>
             </el-aside>
