93afbb62da3f25b88915180af53192b81444da62
aerlany <1243535201@qq.com>
2023-04-14 18:57:41 +0800

    平时成绩管理模块

--- src/components/classTables/usualPreformanceTable.vue
+++ src/components/classTables/usualPreformanceTable.vue
@@ -16,57 +16,42 @@
 
         <el-main>
             <el-table border="true" :header-cell-style="tableHeader" :data="tableData" v-show="!ischoose">
-                <el-table-column label="学号">
-                    <template slot-scope="scope">
+                <el-table-column label="学号" prop="studentNumber">
+                    <!-- <template slot-scope="scope">
                         <el-input v-model="scope.row.studentNumber" v-show="!scope.row.showUser"></el-input>
                         <p v-show="scope.row.showUser">{{ scope.row.studentNumber }}</p>
-                    </template>
+                    </template> -->
                 </el-table-column>
-                <el-table-column label="姓名">
-                    <template slot-scope="scope">
-                       <el-input v-model="scope.row.studentName" v-show="!scope.row.showUser"></el-input> 
+                <el-table-column label="姓名" prop="studentName">
+                    <!-- <template slot-scope="scope">
+                        <el-input v-model="scope.row.studentName" v-show="!scope.row.showUser"></el-input>
                         <p v-show="scope.row.showUser">{{ scope.row.studentName }}</p>
-                    </template>
+                    </template> -->
                 </el-table-column>
-                <el-table-column label="班级">
-                    <template slot-scope="scope">
+                <el-table-column label="班级" prop="className">
+                    <!-- <template slot-scope="scope">
                         <el-input v-model="scope.row.className" v-show="!scope.row.showUser"></el-input>
                         <p v-show="scope.row.showUser">{{ scope.row.className }}</p>
-                    </template>
+                    </template> -->
                 </el-table-column>
-                <el-table-column label="作业分">
-                    <template slot-scope="scope">
-                         <el-input type="number" v-model="scope.row.score" v-show="!scope.row.showScore"></el-input>
-                        <p v-show="scope.row.showScore">{{ scope.row.score }}</p>
-                    </template>
+
+                <el-table-column v-for="item, index in examMethods" :label="item.message" :prop="item.data" :key="index">
+
                 </el-table-column>
-                <el-table-column label="课堂提问分">
+
+                <el-table-column label="操作" width="200px">
                     <template slot-scope="scope">
-                        <el-input type="number" v-model="scope.row.score" v-show="!scope.row.showScore"></el-input>
-                        <p v-show="scope.row.showScore">{{ scope.row.score }}</p>
-                    </template>
-                </el-table-column>
-                <el-table-column label="期中考核成绩">
-                    <template slot-scope="scope">
-                        <el-input type="number" v-model="scope.row.score" v-show="!scope.row.showScore"></el-input>
-                        <p v-show="scope.row.showScore">{{ scope.row.score }}</p>
-                    </template>
-                </el-table-column>
-                <el-table-column label="实验分">
-                    <template slot-scope="scope">
-                        <el-input type="number" v-model="scope.row.score" v-show="!scope.row.showScore"></el-input>
-                        <p v-show="scope.row.showScore">{{ scope.row.score }}</p>
-                    </template>
-                </el-table-column>
-                <el-table-column label="操作" width="170px">
-                    <template slot-scope="scope">
-                        <el-button type="primary"  style="margin: 1vh ;" size="mini" @click="setting(scope.$index)">编辑</el-button>
-                        
-                        <el-button type="danger" style="margin: 1vh ;" size="mini" @click="delectData(scope.$index)">删除</el-button>
+                        <el-button type="primary" style="margin-left: 1vw ;" size="mini"
+                            @click="setting(scope.$index)">编辑</el-button>
+
+                        <el-button type="danger" style="margin-left: 1vw ;" size="mini"
+                            @click="delectData(scope.$index)">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
-            <el-button type="primary" @click="addData">添加</el-button>
+
+            <el-button style="margin-top: 1vw;" type="primary" @click="addData">添加</el-button>
+
             <el-dialog title="编辑" :visible.sync="isShow">
                 <el-form :model="tableData[index]">
                     <el-form-item label="学号" prop="studentNumber">
@@ -78,24 +63,26 @@
                     <el-form-item label="班级" prop="className">
                         <el-input v-model="tableData[index].className"></el-input>
                     </el-form-item>
-                    <el-form-item label="作业分" prop="score">
-                        <el-input v-model="tableData[index].score"></el-input>
+
+                    <el-form-item v-if="isattendanceScore" label="考勤" prop="attendanceScore">
+                        <el-input v-model="tableData[index].attendanceScore"></el-input>
                     </el-form-item>
-                    <el-form-item label="课堂提问分" prop="score">
-                        <el-input v-model="tableData[index].score"></el-input>
+                    <el-form-item v-if="isquizScore" label="课堂提问分" prop="quizScore">
+                        <el-input v-model="tableData[index].quizScore"></el-input>
                     </el-form-item>
-                    <el-form-item label="其中考核成绩" prop="score">
-                        <el-input v-model="tableData[index].score"></el-input>
+                    <el-form-item v-if="ismidTermScore" label="期中考核成绩" prop="midTermScore">
+                        <el-input v-model="tableData[index].midTermScore"></el-input>
                     </el-form-item>
-                    <el-form-item label="试验分" prop="score">
-                        <el-input v-model="tableData[index].score"></el-input>
+                    <el-form-item v-if="isworkScore" label="作业分" prop="workScore">
+                        <el-input v-model="tableData[index].workScore"></el-input>
                     </el-form-item>
-                    
+
+
                 </el-form>
                 <div slot="footer" class="dialog-footer">
                     <el-button type="success" style="margin: 1vh ;" @click="saveData(tableData[index])">保存</el-button>
                     <el-button type="danger" style="margin: 1vh ;" @click="isShow = false">取消</el-button>
-                </div>  
+                </div>
             </el-dialog>
         </el-main>
     </el-container>
@@ -115,9 +102,15 @@ export default {
             courseList: [],
             //表格数据
             tableData: [],
-            dataObj:{studentNumber:"",studentName:"",className:"",score:0,showScore:true,showUser:true},
-            isShow : false,
-            index :0
+            dataObj: { studentNumber: "", studentName: "", className: "", score: 0, showScore: true, showUser: true },
+            isShow: false,
+            index: 0,
+            //考核方式列表遍历表格列
+            examMethods: [],
+            isattendanceScore: false,
+            isworkScore: false,
+            isquizScore: false,
+            ismidTermScore: false,
         }
     },
     methods: {
@@ -135,32 +128,49 @@ export default {
         //获取详细信息
         getStudentScore() {
             api.get("/student/10/getStudent", "", (resp) => {
-               
-                for(let index=0;index<resp.data.data.length;index++){
-                    resp.data.data[index].showUser = true
-                    resp.data.data[index].showScore = true
-                }
+
+                // for (let index = 0; index < resp.data.data.length; index++) {
+                //     resp.data.data[index].showUser = true
+                //     resp.data.data[index].showScore = true
+                // }
+
                 this.tableData = resp.data.data;
-                
+
             })
         },
-        addData(){
+        addData() {
             this.tableData.push(JSON.parse(JSON.stringify(this.dataObj)))
-           
+
         },
-        setting(index1){
-            this.isShow = !this.isShow
-           this.index = index1
+        setting(index1) {
+            this.isShow = !this.isShow;
+            this.index = index1;
+            for (var i = 0; i < this.examMethods.length; i++) {
+                switch (this.examMethods[i].data) {
+                    case "attendanceScore":
+                        this.isattendanceScore = true;
+                        break;
+                    case "workScore":
+                        this.isworkScore = true;
+                        break;
+                    case "quizScore":
+                        this.isquizScore = true;
+                        break;
+                    case "midTermScore":
+                        this.ismidTermScore = true;
+                        break;
+                }
+            }
         },
-        saveData(){
+        saveData() {
             this.$confirm('是否提交 ?', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-                this.isShow = !this.isShow;                                    
-                    api.put("/student/updateStudent", this.tableData[this.index], (resp) => { })                  
-                }
+                this.isShow = !this.isShow;
+                api.put("/student/updateStudent", this.tableData[this.index], (resp) => { })
+            }
             ).catch(() => {
                 this.$message({
                     type: 'info',
@@ -168,44 +178,18 @@ export default {
                 });
             });
         },
-        delectData(index){
+        delectData(index) {
             this.tableData.splice(index, 1);
-        //     if (!this.tableData[index].id) {
-        //     //删除本地
-        //     this.tableData.splice(index, 1);
-        // }
-        // else {
-        //     //删除云端
-        //     this.$confirm('是否提交 ?', '提示', {
-        //     confirmButtonText: '确定',
-        //     cancelButtonText: '取消',
-        //     type: 'warning'
-        //     }).then(() => {
-        //     api.del("/stduent/deleteStudent/" + this.tableData[index].id, "", (resp) => {
-        //         if (resp.data.flag) {
-        //         this.$message({
-        //             type: 'success',
-        //             message: '成功!'
-        //         });
-        //         this.tableData.splice(index, 1);
-        //         } else if (resp.status != 200) {
-        //         this.$message({
-        //             type: 'error',
-        //             message: '失败!'
-        //         });
-        //         }
-        //     })
-        //     }).catch(() => {
-        //     this.$message({
-        //         type: 'info',
-        //         message: '已取消'
-        //     });
-        //     });
-        // }
         },
         test(param) {
             console.log(param);
+        },
 
+        //获取考核方式
+        getExamMethods() {
+            api.get("/student/10/getMethods", "", (resp) => {
+                this.examMethods = resp.data.data;
+            })
         },
 
         //点击课程选择框
@@ -216,6 +200,7 @@ export default {
         },
     },
     mounted() {
+        this.getExamMethods();
         this.getMessage();
         this.getStudentScore();
         if (this.$route.query.id) {
