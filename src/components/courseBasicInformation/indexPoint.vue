2d90d640dbadcf27a8cb29855dedd0158f8eed30
蛐蛐 <2950214626@qq.com>
2023-04-27 15:09:28 +0800

    教学大纲和指标点上传

--- src/components/courseBasicInformation/indexPoint.vue
+++ src/components/courseBasicInformation/indexPoint.vue
@@ -1,10 +1,12 @@
 <template>
-  
+  <pdf-upload pfdType="indexPoint"></pdf-upload>
 </template>
 
 <script>
+import pdfUpload from './pdfUpload.vue'
 export default {
     name:'indexPoint',
+    components:{pdfUpload},
     data(){
         return{
 
