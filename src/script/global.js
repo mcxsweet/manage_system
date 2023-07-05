5ae4bd92dc7900e7cad58c12caa523ecc511e8c4
aerlany <1243535201@qq.com>
2023-07-05 11:56:15 +0800

    运行和部署环境配置

--- src/script/global.js
+++ src/script/global.js
@@ -3,6 +3,7 @@ export default {
     BaseUrl: "http://localhost:8080",
     // BaseUrl: "http://218.194.100.49:8080",
     // BaseUrl: "https://cs6.swfu.edu.cn/zyrz",
+
     // runTiemPath: "/zyrz",
     runTiemPath: ""
 }
