e99a37f1505cde75664c09c7e058493b8a25fa86
aerlany <1243535201@qq.com>
2023-10-31 15:46:43 +0800

    我的小更改

--- src/script/global.js
+++ src/script/global.js
@@ -1,7 +1,7 @@
 export default {
     // 当部署到服务器上时更改
-    BaseUrl: "http://localhost:8080",
-    // BaseUrl: "http://218.194.100.49:8080",
+    // BaseUrl: "http://localhost:8080",
+    BaseUrl: "http://218.194.100.49:8080",
     // BaseUrl: "https://cs6.swfu.edu.cn/zyrz",
 
     // runTiemPath: "/zyrz",
